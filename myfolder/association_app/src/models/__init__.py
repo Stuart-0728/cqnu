b5341750f4837a5ada6cf881038210d1aa760634
Stuart-0728 <1078184866@qq.com>
2025-05-28 19:04:34 +0800

    Update __init__.py

--- myfolder/association_app/src/models/__init__.py
+++ myfolder/association_app/src/models/__init__.py
@@ -3,7 +3,7 @@ from src.models.activity import Activity
 from src.models.registration import Registration
 
 def init_db(app):
-    """初始化数据库"""
+    """初始化数据库并创建默认管理员"""
     db.init_app(app)
     with app.app_context():
         db.create_all()
@@ -16,10 +16,9 @@ def create_default_admin():
     admin = User(
         username='admin',
         email='admin@cqnu.edu.cn',
-        password=default_admin_password,  # 从环境变量获取密码，默认值便于开发环境
+        password=default_admin_password,
         full_name='系统管理员',
         role='admin'
-        # 移除 force_password_change=True 参数，因为User模型不接受此参数
     )
     db.session.add(admin)
     db.session.commit()
