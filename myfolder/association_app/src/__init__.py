02f74b441c7d709860be7a713a5ee827d3b8e291
Stuart-0728 <1078184866@qq.com>
2025-05-28 19:32:56 +0800

    Update __init__.py

--- myfolder/association_app/src/__init__.py
+++ myfolder/association_app/src/__init__.py
@@ -1,15 +1,17 @@
-# association_app/src/models/__init__.py
 from flask_sqlalchemy import SQLAlchemy
+
+# 全局 SQLAlchemy 实例
 
 db = SQLAlchemy()
 
-# 导入模型
+# 导入模型类，确保被 SQLAlchemy 识别
 from src.models.user import User
 from src.models.activity import Activity
 from src.models.registration import Registration
 
+
 def init_db(app):
-    """仅初始化数据库并创建所有表"""
+    """初始化数据库，创建所有表"""
     db.init_app(app)
     with app.app_context():
         db.create_all()
